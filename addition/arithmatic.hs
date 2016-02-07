b944422c962c13b6c3ad0923e4553f7eed9e47e9
Andrew Newman <andrewfnewman@gmail.com>
2016-02-07 20:52:45 +1000

    Moar types

--- addition/arithmatic.hs
+++ addition/arithmatic.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Arithmatic where 
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Function
 import Data.List (sort)
 
 half :: Double -> Double
@@ -13,36 +16,31 @@ halfIdentity = (*2) . half
 listOrdered :: (Ord a) => [a] -> Bool
 listOrdered xs = snd $ foldr go (Nothing, True) xs
   where go y (Nothing, t) = (Just y, t) 
-        go y (Just x, t) = (Just y, x >= y)
+        go y (Just x, _) = (Just y, x >= y)
 
 main :: IO () 
 main = hspec $ do
   describe "Arithmatic" $ do
       it "half" $ do
-        property $ \x -> (halfIdentity x) == (x :: Double)
+        property $ \(x::Double) -> (halfIdentity x) == x
       it "list" $ do
-        property $ \x -> listOrdered (sort x::[Int]) == True
+        property $ \(x::[Int]) -> listOrdered (sort x) == True
       it "plus associative" $ do
-        property $ \(x,y,z) -> x + (y + z) == (((x + y) + z)::Int) 
+        property $ \((x,y,z)::(Int,Int,Int)) -> x + (y + z) == (x + y) + z 
       it "plus commute" $ do
-        property $ \(x,y) -> x + y == ((y + x)::Int) 
+        property $ \((x,y)::(Int,Int)) -> x + y == y + x
       it "mult associative" $ do
-        property $ \(x,y,z) -> x * (y * z) == (((x * y) * z)::Int) 
+        property $ \((x,y,z)::(Int,Int,Int)) -> x * (y * z) == (x * y) * z 
       it "mult commute" $ do
-        property $ \(x,y) -> x * y == ((y * x)::Int) 
+        property $ \((x,y)::(Int,Int)) -> x * y == y * x 
       it "quot commute" $ do
-        property $ \(x,y) -> (quot x y) * y + (rem x y) == (x::Int) 
+        property $ \((NonZero x, NonZero y)::(NonZero Int, NonZero Int)) -> (quot x y) * y + (rem x y) == x 
       it "div commute" $ do
-        property $ \(x,y) -> (div x y) * y + (mod x y) == (x::Int) 
-       
-    --   it "1 + 1 is greater than 1" $ do
-    --     (1 + 1) > 1 `shouldBe` True
-        
-    --   it "2 + 2 is equal to 4" $ do 
-    --     2 + 2 `shouldBe` 4
-        
-    --   it "15 divided by 3 is 5" $ do
-    --     dividedBy 15 3 `shouldBe` (5, 0)
-        
-    --   it "22 divided by 5 is 4 remainder 2" $ do
-    --     dividedBy 22 5 `shouldBe` (4, 2)
+        property $ \((NonZero x, NonZero y)::(NonZero Int, NonZero Int)) -> (div x y) * y + (mod x y) == x 
+      it "^ associative" $ do
+        property $ \((x,y,z)::(Int,Int,Int)) -> x ^ (y ^ z) == (x ^ y) ^ z
+      it "^ commute" $ do
+        property $ \((x,y)::(Int,Int)) -> x ^ y == y ^ x
+      it "same twice list" $ do
+        property $ \(x::[Int]) -> (reverse . reverse) x == id x
+                        
