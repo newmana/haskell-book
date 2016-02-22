bcf41087fa1695c532af0858534f823659018710
Andrew Newman <andrewfnewman@gmail.com>
2016-02-23 05:42:53 +1000

    Blah

--- trivialSemigroup.hs
+++ trivialSemigroup.hs
@@ -29,25 +29,55 @@ instance Arbitrary Trivial where
   arbitrary = return Trivial
 
 instance (Arbitrary a) => Arbitrary (Identity a) where
-  arbitrary = do
-    a <- arbitrary
-    return (Identity a)
+  arbitrary = Identity <$> arbitrary
 
 identitySmush :: (Semigroup a) => Identity a -> Identity a -> Identity a
 identitySmush = (<>)
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
-  arbitrary = do
-    a <- arbitrary
-    b <- arbitrary
-    return (Two a b)
+  arbitrary = Two <$> arbitrary <*> arbitrary
 
 instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = Three <$> arbitrary <*> arbitrary <*> arbitrary
+
+newtype BoolConj = BoolConj Bool deriving (Show, Eq)
+newtype BoolDisj = BoolDisj Bool deriving (Show, Eq)
+
+instance Semigroup BoolConj where
+  (<>) (BoolConj True) (BoolConj True) = (BoolConj True)
+  (<>) _ _ = (BoolConj False)
+
+instance Semigroup BoolDisj where
+  (<>) (BoolDisj False) (BoolDisj False) = (BoolDisj False)
+  (<>) _ _ = (BoolDisj True)
+
+instance Arbitrary BoolConj where
+  arbitrary = fmap BoolConj arbitrary
+
+instance Arbitrary BoolDisj where
+  arbitrary = fmap BoolDisj arbitrary
+
+data Or a b = Fst a | Snd b deriving (Show, Eq)
+
+instance Semigroup (Or a b) where
+  (<>) (Fst a) (Fst b) = Fst b
+  (<>) (Fst a) (Snd b) = Snd b
+  (<>) (Snd a) (Fst b) = Snd a
+  (<>) (Snd a) (Snd b) = Snd a
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
   arbitrary = do
     a <- arbitrary
     b <- arbitrary
-    c <- arbitrary
-    return (Three a b c)
+    elements [(Fst a), (Snd b)]
+
+f = Combine $ \n -> Sum (n+1)
+g = Combine $ \n -> Sum (n-1)
+
+newtype Combine a b = Combine { unCombine :: (a -> b) }
+
+instance (Semigroup a, Semigroup b) => Semigroup (Combine a b) where
+  (<>) f@(Combine a) g@(Combine b) = f
 
 main :: IO ()
 main = do
@@ -55,5 +85,8 @@ main = do
   -- quickCheck (monoidAssoc :: (Eq a, Show a) => Trivial -> Trivial -> Trivial -> Bool)
   -- quickCheck (monoidAssoc :: (Eq a, Show a, Semigroup a) => Identity a -> Identity a -> Identity a -> Bool)
   -- quickCheck (monoidAssoc :: (Eq a, Show a, Eq b, Show b, Semigroup a, Semigroup b) => Two a b -> Two a b -> Two a b -> Bool)
-  -- quickCheck (monoidAssoc :: (Eq a, Show a, Eq b, Show b, Eq c, Show c) => Three a b c -> Three a b c -> Three a b c -> Bool)
+  -- quickCheck (monoidAssoc :: (Eq a, Show a, Eq b, Show b, Eq c, Show c, Semigroup a, Semigroup b, Semigroup c) => Three a b c -> Three a b c -> Three a b c -> Bool)
+  -- quickCheck (monoidAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
+  -- quickCheck (monoidAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Bool)
+  -- quickCheck (monoidAssoc :: (Eq a, Show a, Eq b, Show b) => Or a b -> Or a b -> Or a b -> Bool)
   return ()
