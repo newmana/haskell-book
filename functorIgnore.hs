d1641ecf51987aee5bd2a44963292998e0790592
Andrew Newman <andrewfnewman@gmail.com>
2016-06-26 22:02:53 +1000

    Chapter 16

--- functorIgnore.hs
+++ functorIgnore.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes        #-}
+
 incIfJust :: Num a => Maybe a -> Maybe a
 incIfJust (Just n) = Just $ n + 1
 incIfJust Nothing = Nothing
@@ -7,10 +10,10 @@ showIfJust (Just s) = Just $ show s
 showIfJust Nothing = Nothing
 
 incMaybe :: Num a => Maybe a -> Maybe a
-incMaybe m = fmap (+1) m
+incMaybe = fmap (+ 1)
 
 showMaybe :: Show a => Maybe a -> Maybe String
-showMaybe s = fmap show s
+showMaybe = fmap show
 
 incMaybe'' :: Num a => Maybe a -> Maybe a
 incMaybe'' = fmap (+1)
@@ -39,10 +42,10 @@ showIfRight (Right s) = Right $ show s
 showIfRight (Left e) = Left e
 
 incEither :: Num a => Either e a -> Either e a
-incEither m = fmap (+1) m
+incEither = fmap (+ 1)
 
 showEither :: Show a => Either e a -> Either e String
-showEither s = fmap show s
+showEither = fmap show
 
 incEither' :: Num a => Either e a -> Either e a
 incEither' = fmap (+1)
@@ -60,3 +63,49 @@ newtype Constant a b = Constant { getConstant :: a } deriving (Eq, Show)
 
 instance Functor (Constant m) where
   fmap _ (Constant v) = Constant v
+
+data Wrap f a =
+  Wrap (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (Wrap f) where
+  fmap f (Wrap fa) = Wrap (fmap f fa)
+
+getInt :: IO Int
+getInt = fmap read getLine
+
+meTooIsm :: IO String
+meTooIsm = do
+  input <- getLine
+  return (input ++ "and me too!")
+
+andThat :: IO String
+andThat = fmap (++ " yeah and that") getLine
+
+bumpIt :: IO Int
+bumpIt = do
+  intVal <- getInt
+  return (intVal + 1)
+
+type Nat f g = forall a . f a -> g a
+
+maybeToList :: Nat Maybe []
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
+
+type Nat' f g a = f a -> g a
+
+maybeToList' :: Nat' Maybe [] a
+maybeToList' Nothing = []
+maybeToList' (Just a) = [a]
+
+data Tuple a b =
+  Tuple a b
+  deriving (Eq, Show)
+
+newtype Flip f a b =
+  Flip (f b a)
+  deriving (Eq, Show)
+
+instance Functor (Flip Tuple a) where
+  fmap (+1) (Flip (Tuple 1 "blah"))fmap f (Flip (Tuple a b)) = Flip $ Tuple (f a) b
